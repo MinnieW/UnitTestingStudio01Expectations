922ea91430d6158b076b7c5288cfadafe2e097ec
MinnieW <84355273+MinnieW@users.noreply.github.com>
2021-07-01 21:40:37 +0000

    Dry out code by using an object. Comment out either version

--- launchCodeRocks.js
+++ launchCodeRocks.js
@@ -1,4 +1,4 @@
-let number = 6;
+let number = 7;
 
 let divisibleArray = [2,3,5,7];
 
@@ -8,38 +8,69 @@ function divisible(num){
 
 let newArray = divisibleArray.map(divisible);
 
-function launchOutput(arr){
-  if (arr[0] === 0 && (arr[1] !== 0 &&arr[2] !== 0 && arr[3] !== 0)){
-    return "Launch!"
-  }
+// function launchOutput(arr){
+//   if (arr[0] === 0 && (arr[1] !== 0 &&arr[2] !== 0 && arr[3] !== 0)){
+//     return "Launch!"
+//   }
 
-  if (arr[1] === 0 && (arr[0] !== 0 && arr[2] !== 0 && arr[3] !== 0)){
-    return "Code!"
-  }
+//   if (arr[1] === 0 && (arr[0] !== 0 && arr[2] !== 0 && arr[3] !== 0)){
+//     return "Code!"
+//   }
 
-  if (arr[2] === 0 && (arr[0] !== 0 &&arr[1] !== 0 && arr[3] !== 0)){
-    return "Rocks!"
-  }  
+//   if (arr[2] === 0 && (arr[0] !== 0 &&arr[1] !== 0 && arr[3] !== 0)){
+//     return "Rocks!"
+//   }  
 
-  if ((arr[0] === 0 && arr[1] === 0) && (arr[2] !== 0 && arr[3] !== 0)){
-    return "LaunchCode!"
-  }
+//   if ((arr[0] === 0 && arr[1] === 0) && (arr[2] !== 0 && arr[3] !== 0)){
+//     return "LaunchCode!"
+//   }
 
-  if ((arr[1] === 0 && arr[2] === 0) && (arr[0] !== 0 && arr[3] !== 0)){
-    return "Code Rocks!"
-  }
+//   if ((arr[1] === 0 && arr[2] === 0) && (arr[0] !== 0 && arr[3] !== 0)){
+//     return "Code Rocks!"
+//   }
 
-  if ((arr[0] === 0 && arr[2] === 0) && (arr[1] !== 0 && arr[3] !== 0)){
-    return "Launch Rocks! (CRASH!!!!)"
-  }  
+//   if ((arr[0] === 0 && arr[2] === 0) && (arr[1] !== 0 && arr[3] !== 0)){
+//     return "Launch Rocks! (CRASH!!!!)"
+//   }  
 
-    if (arr[0] === 0 && arr[1] === 0 && arr[2] === 0){
-    return "LaunchCode Rocks!"
-  } 
+//     if (arr[0] === 0 && arr[1] === 0 && arr[2] === 0){
+//     return "LaunchCode Rocks!"
+//   } 
 
-return "Rutabagas! That doesn't work."
+// return "Rutabagas! That doesn't work."
 
-}
+// }
 //console.log(launchOutput(newArray))
+
+let divNumberObject = {};
+
+for (i=0; i < newArray.length; i++) {
+  divNumberObject[divisibleArray[i]] = newArray[i]
+}
+
+function launchOutput(obj){
+  let out = "";
+  if (obj["2"] === 0){
+    out += "Launch";
+  }  
+
+  if (obj["3"] === 0){
+  out += "Code";  
+  }
+  
+  if (obj["5"] === 0){
+  out += " Rocks";  
+  }
+
+  if (out === ""){
+  return "Rutabagas! That doesn't work."
+  } else if (out === "Launch Rocks"){
+    return out + "! (CRASH!!!!)"
+  } else {
+    return out.trim() +"!"
+  }
+}
+//console.log(divNumberObject)
+//console.log(newLaunchOutput(divNumberObject))
 
 module.exports = launchOutput;
--- spec/launchCodeRocks.spec.js
+++ spec/launchCodeRocks.spec.js
@@ -1,52 +1,107 @@
 const launchOutput = require('../launchCodeRocks.js');
 
+// describe("launchOutput", function(){
+
+//   it("A number ONLY divisible by 2, launchOutput returns 'Launch!'",function(){
+//     let array = [0,3,4,5];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Launch!");
+//     // let object = {2: 0}
+//     // let output = launchOutput(object)
+//     // expect(output).toEqual("Launch!")
+//   });
+
+//   it("A number ONLY divisible by 3, launchOutput returns 'Code!'",function(){
+//     let array = [2,0,3,4];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Code!");
+//   })
+
+//   it("A number ONLY divisible by 5, launchOutput returns 'Rocks!'",function(){
+//     let array = [2,2,0,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Rocks!");
+//   })
+
+//   it("A number divisible by 2 AND 3, launchOutput returns 'LaunchCode!'", function(){
+//     let array = [0,0,2,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("LaunchCode!");
+//   })
+
+//   it("A number divisible by 3 AND 5, return 'Code Rocks!'",function(){
+//     let array = [2,0,0,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Code Rocks!");
+//   })
+
+//   it("A number divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)!'",function(){
+//     let array = [0,2,0,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Launch Rocks! (CRASH!!!!)");
+//   })
+
+//   it("A number divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'",function(){
+//     let array = [0,0,0,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("LaunchCode Rocks!");
+//   })
+
+//   it("A number NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'",function(){
+//     let array = [2,2,2,2];
+//     let output = launchOutput(array);
+//     expect(output).toEqual("Rutabagas! That doesn't work.")
+//   })
+// });
+
+
 describe("launchOutput", function(){
 
-  it("When passed a number that is ONLY divisible by 2, launchOutput returns 'Launch!'",function(){
-    let array = [0,3,4,5];
-    let output = launchOutput(array);
-    expect(output).toEqual("Launch!");
+  it("A number ONLY divisible by 2, launchOutput returns 'Launch!'",function(){
+    let object = {2: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("Launch!")
   });
 
-  it("When passed a number that is ONLY divisible by 3, launchOutput returns 'Code!'",function(){
-    let array = [2,0,3,4];
-    let output = launchOutput(array);
-    expect(output).toEqual("Code!");
+  it("A number ONLY divisible by 3, launchOutput returns 'Code!'",function(){
+    let object = {3: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("Code!")
   })
 
-  it("When passed a number that is ONLY divisible by 5, launchOutput returns 'Rocks!'",function(){
-    let array = [2,2,0,2];
-    let output = launchOutput(array);
-    expect(output).toEqual("Rocks!");
+  it("A number ONLY divisible by 5, launchOutput returns 'Rocks!'",function(){
+    let object = {5: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("Rocks!")
   })
 
-  it("When passed a number that is divisible by 2 AND 3, launchOutput returns 'LaunchCode!'", function(){
-    let array = [0,0,2,2];
-    let output = launchOutput(array);
-    expect(output).toEqual("LaunchCode!");
+  it("A number divisible by 2 AND 3, launchOutput returns 'LaunchCode!'", function(){
+    let object = {2: 0, 3: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("LaunchCode!")
   })
 
-  it("When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'",function(){
-    let array = [2,0,0,2];
-    let output = launchOutput(array);
-    expect(output).toEqual("Code Rocks!");
+  it("A number divisible by 3 AND 5, return 'Code Rocks!'",function(){
+    let object = {5: 0, 3: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("Code Rocks!")
   })
 
-  it("When passed a number that is divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)!'",function(){
-    let array = [0,2,0,2];
-    let output = launchOutput(array);
-    expect(output).toEqual("Launch Rocks! (CRASH!!!!)");
+  it("A number divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)'",function(){
+    let object = {2: 0, 5: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("Launch Rocks! (CRASH!!!!)")
   })
 
-  it("When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'",function(){
-    let array = [0,0,0,2];
-    let output = launchOutput(array);
-    expect(output).toEqual("LaunchCode Rocks!");
+  it("A number divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'",function(){
+    let object = {2: 0, 3: 0, 5: 0}
+    let output = launchOutput(object)
+    expect(output).toEqual("LaunchCode Rocks!")
   })
 
-  it("When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'",function(){
-    let array = [2,2,2,2];
-    let output = launchOutput(array);
+  it("A number NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'",function(){
+    let object = {2: 1, 3: 1, 5: 1}
+    let output = launchOutput(object)
     expect(output).toEqual("Rutabagas! That doesn't work.")
   })
 });
